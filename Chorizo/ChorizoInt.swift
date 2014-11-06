a20e85175a8f45987a3147ddb292c2078a74c85e
Vandad Nahavandipoor <vandad.nahavandipoor@schibsted.se>
2014-11-06 11:09:19 +0100

    Added the ChorizoInt class

---
+++ Chorizo/ChorizoInt.swift
@@ -0,0 +1,25 @@
+//
+//  ChorizoInt.swift
+//  Chorizo
+//
+//  Created by Vandad Nahavandipoor on 06/11/14.
+//  Copyright (c) 2014 Pixolity Ltd. All rights reserved.
+//
+
+import Foundation
+
+extension Int{
+    
+    func toString() -> String{
+        return "\(self)"
+    }
+    
+    func toArray() -> [Int]{
+        return [self]
+    }
+    
+    func toArray(repeat: Int) -> [Int]{
+        return [Int](count: repeat, repeatedValue: self)
+    }
+    
+}
