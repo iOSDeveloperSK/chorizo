27ca62d0d393e4f7eaec4bcea038de53a379fa01
Vandad Nahavandipoor <vandad.np@gmail.com>
2014-11-06 12:16:54 +0100

    Merged

--- Chorizo/ChorizoString.swift
+++ Chorizo/ChorizoString.swift
@@ -7,15 +7,70 @@
 //
 
 import Foundation
+import UIKit
 
 extension String{
   
-  public func toArray() -> [String]{
+  func toArray() -> [String]{
     return [self]
   }
   
-  public func and(anotherString: String) -> [String]{
+  func and(anotherString: String) -> [String]{
     return [self, anotherString]
+  }
+  
+}
+
+/* Downloading */
+
+extension String{
+  
+  /**
+  Downloads and returns the data. Good for returning images and stuff like that
+  */
+  func download(completionBlock: (data: NSData?) -> ()) -> String{
+    
+    ChorizoAsyncUrlConnection(url: self).onSuccess { (data: NSData) -> () in
+      completionBlock(data: data)
+      }
+      .onFailure { (error: NSError) -> () in
+        completionBlock(data: nil)
+      }
+      .start()
+    
+    return self
+  }
+  
+  /**
+  Downloads self and returns the downloaded data as string.
+  Good for downloading a web page
+  */
+  func download(completionBlock: (string: String?) -> ()) -> String{
+    
+    self.download { (data: NSData?) -> () in
+      if let data = data{
+        let string = NSString(data: data, encoding: 0)
+        completionBlock(string: string)
+      } else {
+        completionBlock(string: nil)
+      }
+    }
+    
+    return self
+  }
+  
+  func download(completionBlock: (image: UIImage?) -> ()) -> String{
+    
+    self.download { (data: NSData?) -> () in
+      if let data = data{
+        let image = UIImage(data: data)
+        completionBlock(image: image)
+      } else {
+        completionBlock(image: nil)
+      }
+    }
+    
+    return self
   }
   
 }
